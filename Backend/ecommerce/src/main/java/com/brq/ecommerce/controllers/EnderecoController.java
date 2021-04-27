ab5328034e24fa9db2287e8a54fd5285b0510adf
Gustavo Casco Zacharias <gustavocasco@brq.com>
2021-04-27 17:58:01 -0300

    Inclusão do metodo patch :smile:

--- Backend/ecommerce/src/main/java/com/brq/ecommerce/controllers/EnderecoController.java
+++ Backend/ecommerce/src/main/java/com/brq/ecommerce/controllers/EnderecoController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +35,12 @@ public class EnderecoController {
 	public ResponseEntity< EnderecoDTO > findOne( @PathVariable("id") int id  ) {
 		
 		return ResponseEntity.ok().body( this.enderecoService.findOne(id) );						
+	}
+	
+	@PatchMapping("/{id}")
+	public ResponseEntity<EnderecoDTO> update (@PathVariable int id, @RequestBody EnderecoDTO attEndereco)
+	{
+		return ResponseEntity.ok(this.enderecoService.update(id, attEndereco));
 	}
 	
 	
--- Backend/ecommerce/src/main/java/com/brq/ecommerce/services/EnderecoService.java
+++ Backend/ecommerce/src/main/java/com/brq/ecommerce/services/EnderecoService.java
@@ -3,6 +3,7 @@ package com.brq.ecommerce.services;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -33,5 +34,27 @@ public class EnderecoService {
 		return this.enderecoRepository.findById(id)
 				.orElseThrow( () -> new RuntimeException("Endereco não encontrado")  ).toDto();
 	}
+	
+	public EnderecoDTO update(int id, EnderecoDTO attEndereco)
+
+	{						
+		Optional<EnderecoModel> endereco = this.enderecoRepository.findById(id);
+		if(endereco.isPresent())
+		{
+			EnderecoModel obj = endereco.get();
+			obj.setLogradouro(attEndereco.getLogradouro());
+			obj.setCep(attEndereco.getCep());
+			obj.setEstado(attEndereco.getEstado());
+			obj.setNumero(attEndereco.getNumero());
+			obj.setComplemento(attEndereco.getComplemento());
+			obj.setCidade(attEndereco.getCidade());
+			return this.enderecoRepository.save(obj).toDto();
+		}else
+		{
+			throw new RuntimeException("Aluno não encontrada");
+		}
+		
+	}
+	
 
 }
