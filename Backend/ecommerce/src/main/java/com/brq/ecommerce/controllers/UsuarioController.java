9c118554bfa8496e54b40be45eb22fef19c49aa4
Bruno da Silva Mota <bmota@brq.com>
2021-04-27 17:03:37 -0300

    Arrumando duplicada

--- Backend/ecommerce/src/main/java/com/brq/ecommerce/controllers/UsuarioController.java
+++ Backend/ecommerce/src/main/java/com/brq/ecommerce/controllers/UsuarioController.java
@@ -1,42 +1,14 @@
 package com.brq.ecommerce.controllers;
+import java.util.List;
 
-<<<<<<< HEAD
-
-import com.brq.ecommerce.dtos.UsuarioDTO;
-import com.brq.ecommerce.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
-@RequestMapping("usuarios")
-@RestController
-@CrossOrigin(origins = "*")
-public class UsuarioController {
-
-    @Autowired
-    UsuarioService usuarioService;
-
-    @PatchMapping("/{idUsuario}")
-    public ResponseEntity<UsuarioDTO> update(@Valid @PathVariable int idUsuario, @RequestBody UsuarioDTO updateUsuario) {
-
-        return ResponseEntity.ok().body( this.usuarioService.update(idUsuario, updateUsuario));
-    }
-}
-
-
-=======
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.brq.ecommerce.dtos.UsuarioDTO;
 import com.brq.ecommerce.services.UsuarioService;
+
+import javax.validation.Valid;
 
 
 @RestController
@@ -56,5 +28,10 @@ public class UsuarioController {
 		return this.usuarioService.findOne(idUsuario);
 	}
 
+	@PatchMapping("/{idUsuario}")
+	public ResponseEntity<UsuarioDTO> update(@Valid @PathVariable int idUsuario, @RequestBody UsuarioDTO updateUsuario) {
+
+		return ResponseEntity.ok().body( this.usuarioService.update(idUsuario, updateUsuario));
+	}
+
 }
->>>>>>> a1da7034a2d0a9943994971a827bd03a8df85d98
--- Backend/ecommerce/src/main/java/com/brq/ecommerce/services/UsuarioService.java
+++ Backend/ecommerce/src/main/java/com/brq/ecommerce/services/UsuarioService.java
@@ -1,6 +1,6 @@
 package com.brq.ecommerce.services;
 
-<<<<<<< HEAD
+
 import com.brq.ecommerce.dtos.UsuarioDTO;
 import com.brq.ecommerce.models.UsuarioModel;
 import com.brq.ecommerce.repositories.UsuarioRepository;
@@ -8,18 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-=======
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.brq.ecommerce.dtos.UsuarioDTO;
-import com.brq.ecommerce.models.UsuarioModel;
-import com.brq.ecommerce.repositories.UsuarioRepository;
->>>>>>> a1da7034a2d0a9943994971a827bd03a8df85d98
 
 @Service
 public class UsuarioService {
@@ -35,9 +26,6 @@ public class UsuarioService {
 	public UsuarioDTO findOne(int idUsuario) {
 		return this.usuarioRepository.findById(idUsuario).orElseThrow(()-> new RuntimeException("Usuário não encontrado!")).toDTO();
 	}
-
-    @Autowired
-    public UsuarioRepository usuarioRepository;
 
     public UsuarioDTO update(int idUsuario, UsuarioDTO updateUsuario) {
         Optional<UsuarioModel> usuarioOptional = this.usuarioRepository.findById(idUsuario);
