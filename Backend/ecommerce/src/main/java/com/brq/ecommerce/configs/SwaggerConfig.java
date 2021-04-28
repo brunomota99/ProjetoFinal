db55439864efae8ec4777c65eb918f4082740aee
iadr-exe <igoralvesrocha@gmail.com>
2021-04-28 16:33:31 -0300

    feat: inclusão do swagger

---
+++ Backend/ecommerce/src/main/java/com/brq/ecommerce/configs/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.brq.ecommerce.configs;
+
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+				
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("TITULO").description("DESCRIÇÃO").version("VERSÃO")
+				.license("Apache License Version 2.0").licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+				.contact(new Contact("Q", "http://localhost:4200", "email@brq.com")).build();
+	}
+
+}
