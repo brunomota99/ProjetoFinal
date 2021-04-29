c7d76015ed7b8e6c7ee14127c03c6507b0a0b8ac
guithepc <guilherme.pc@yahoo.com>
2021-04-29 16:39:03 -0300

    feat: add update itemPedido

--- Backend/ecommerce/src/main/java/com/brq/ecommerce/controllers/ItemPedidoController.java
+++ Backend/ecommerce/src/main/java/com/brq/ecommerce/controllers/ItemPedidoController.java
@@ -1,9 +1,16 @@
 package com.brq.ecommerce.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.brq.ecommerce.dtos.ItemPedidoDTO;
 import com.brq.ecommerce.services.ItemPedidoService;
 
 @Controller
@@ -14,6 +21,9 @@ public class ItemPedidoController {
 	@Autowired
 	ItemPedidoService itemPedidoService;
 	
-	
+	@PatchMapping("/idItemPedido")
+	public ResponseEntity<ItemPedidoDTO> update (@Valid @RequestBody ItemPedidoDTO newObj, @PathVariable int idItemPedido){
+		return ResponseEntity.ok().body(this.itemPedidoService.update(idItemPedido, newObj));
+	}
 
 }
