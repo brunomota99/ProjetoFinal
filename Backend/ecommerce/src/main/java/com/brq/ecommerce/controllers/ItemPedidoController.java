acb9752f02297627c374c0a9443e1c9fb8f77245
rodrigoaa1988 <rodrigoanjosalmeida@gmail.com>
2021-04-29 15:40:48 -0300

    adicionando método findAll para item-pedido

--- Backend/ecommerce/src/main/java/com/brq/ecommerce/controllers/ItemPedidoController.java
+++ Backend/ecommerce/src/main/java/com/brq/ecommerce/controllers/ItemPedidoController.java
@@ -1,9 +1,14 @@
 package com.brq.ecommerce.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.brq.ecommerce.dtos.ItemPedidoDTO;
 import com.brq.ecommerce.services.ItemPedidoService;
 
 @Controller
@@ -14,6 +19,9 @@ public class ItemPedidoController {
 	@Autowired
 	ItemPedidoService itemPedidoService;
 	
-	
+	@GetMapping("")
+	public ResponseEntity< List<ItemPedidoDTO> > findAll(){
+		return ResponseEntity.ok().body(this.itemPedidoService.findAll());
+	}
 
 }
--- Backend/ecommerce/src/main/java/com/brq/ecommerce/services/ItemPedidoService.java
+++ Backend/ecommerce/src/main/java/com/brq/ecommerce/services/ItemPedidoService.java
@@ -1,8 +1,14 @@
 package com.brq.ecommerce.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.brq.ecommerce.dtos.ItemPedidoDTO;
+import com.brq.ecommerce.models.ItemPedidoModel;
 import com.brq.ecommerce.repositories.ItemPedidoRepository;
 
 @Service
@@ -10,5 +16,15 @@ public class ItemPedidoService {
 	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	
+	public List<ItemPedidoDTO> findAll(){
+		List<ItemPedidoModel> list = this.itemPedidoRepository.findAll();
+		return list.stream().map(x -> x.toDto()).collect(Collectors.toCollection(ArrayList::new));
+	}
+	
+	
+	
+	
 
 }
