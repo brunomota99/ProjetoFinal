c752e0a145a7e6c99a534947629deda7115cc41e
Hugo Souza Araujo <hsaraujo98@outlook.com>
2021-04-29 18:23:05 -0300

    implementação do formulário para cadastro de usuário

--- FrontEnd/Angular/frontend/src/app/shared/shared.module.ts
+++ FrontEnd/Angular/frontend/src/app/shared/shared.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import {HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-NgModule({
+@NgModule({
   declarations: [],
   imports: [
     CommonModule,
     RouterModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
-  exports: [FormsModule],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
 })
+
 export class SharedModule { }
--- FrontEnd/Angular/frontend/src/app/usuario/usuario-routing.module.ts
+++ FrontEnd/Angular/frontend/src/app/usuario/usuario-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuarioFormComponent } from './usuario-form/usuario-form.component';
 import { UsuarioListComponent } from './usuario-list/usuario-list.component';
 
 const routes: Routes = [
-  {path: '', component: UsuarioListComponent }
+  {path: '', component: UsuarioListComponent },
+  {path: 'formulario', component: UsuarioFormComponent }
 ];
 
 @NgModule({
