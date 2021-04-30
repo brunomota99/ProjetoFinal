a3148b74e74c4ad59216fdfa1c16e247330b1728
joaopedroprog <joaopedroprog@outlook.com>
2021-04-30 11:54:20 -0300

    Criado component FieldErrorComponent

--- FrontEnd/Angular/frontend/src/app/shared/shared.module.ts
+++ FrontEnd/Angular/frontend/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { FieldErrorComponent } from './field-error/field-error.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    FieldErrorComponent
   ],
   imports: [
     CommonModule
