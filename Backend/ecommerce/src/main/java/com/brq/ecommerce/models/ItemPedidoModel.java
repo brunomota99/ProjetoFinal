8c48641ea481669d7c6fe518efa76aafc23436c2
Gabriela Almeida <gabrielabrazolin@outlook.com>
2021-04-30 18:05:00 -0300

    fix: importação manytoone item pedido

--- Backend/ecommerce/src/main/java/com/brq/ecommerce/models/ItemPedidoModel.java
+++ Backend/ecommerce/src/main/java/com/brq/ecommerce/models/ItemPedidoModel.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
