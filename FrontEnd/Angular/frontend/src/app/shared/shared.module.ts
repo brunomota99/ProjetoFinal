3dc98af33b552da19b3eb5755dc43492d7e3a41e
joaopedroprog <75503413+joaopedroprog@users.noreply.github.com>
2021-05-03 14:55:19 -0300

    Relizado ajustes nas importações

--- FrontEnd/Angular/frontend/src/app/shared/shared.module.ts
+++ FrontEnd/Angular/frontend/src/app/shared/shared.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FieldErrorComponent } from './field-error/field-error.component';
-
-
-=======
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
->>>>>>> 0e90ccec8f8a574a7926f8e5f7e381deb8807c5d
+
+
+
 
 @NgModule({
   declarations: [
@@ -30,16 +28,11 @@ import { NgxPaginationModule } from 'ngx-pagination';
   exports: [
     FormsModule,
     ReactiveFormsModule,
-    NgxPaginationModule
-  ],
-<<<<<<< HEAD
-  exports : [
+    NgxPaginationModule,
     NavbarComponent,
     FooterComponent,
     FieldErrorComponent
   ]
-=======
->>>>>>> 0e90ccec8f8a574a7926f8e5f7e381deb8807c5d
 })
 
 export class SharedModule { }
