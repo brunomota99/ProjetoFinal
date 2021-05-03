8ef16fc524ee16acd8a4eae712a50c0dad22c59a
Gabriela Almeida <gabrielabrazolin@outlook.com>
2021-05-03 17:13:43 -0300

    fix: dtos e ids

--- Backend/ecommerce/src/main/java/com/brq/ecommerce/dtos/ItemPedidoDTO.java
+++ Backend/ecommerce/src/main/java/com/brq/ecommerce/dtos/ItemPedidoDTO.java
@@ -1,18 +1,13 @@
 package com.brq.ecommerce.dtos;
 
-import javax.validation.constraints.NotNull;
-
 import lombok.Data;
 
 @Data
 public class ItemPedidoDTO {
 	
-	@NotNull
 	private Integer qtdeItemPedido;
 	
-	@NotNull
 	private Double precoItemPedido;
 	
-	@NotNull
 	private ProdutoDto produto;
 }
